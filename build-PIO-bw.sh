f84989108ab496259a62e67ef5b20dd5288c00d7
hdffede <fede@hdfgroup.org>
2015-10-29 16:11:58 -0500

    updated  bash call and control flow
    enable the default bash execution
    modified control flow to include default (nothing happens in that case)
    and use what is currently setup in the user env/modules

--- build-PIO-bw.sh
+++ build-PIO-bw.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 #   ___  __           _      __     __
 #  / _ )/ /_ _____   | | /| / /__ _/ /____ _______
 # / _  / / // / -_)  | |/ |/ / _ `/ __/ -_) __(_-<
@@ -7,27 +7,27 @@
 #
 # Build script to build PIO on Blue Waters
 
-source /opt/modules/default/init/bash
 #####################################
 # CONFIG
 #####################################
-#
-#$COMPILER = "Default"
-$COMPILER = "Intel"
-#$COMPILER = "PGI"
+##source /opt/modules/default/init/bash
+
+#$COMPILER = 'Default'
+COMPILER="Intel"
+#$COMPILER = 'PGI'
 
 #####################################
 # MODULES
 #####################################
 module list
 # select module based on compiler preference
-if [$COMPILER eq 'Intel']; then
+if [$COMPILER -eq "Intel"]; then
   module swap PrgEnv-cray PrgEnv-intel
 else
-  if [$COMPILER eq 'PGI']; then
+  if [$COMPILER -eq "PGI"]; then
     module swap PrgEnv-cray PrgEnv-pgi
   else
-    $COMPILER = "Default"
+    COMPILER="Default"
   fi
 fi
 
